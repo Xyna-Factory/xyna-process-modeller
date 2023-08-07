5e944c1a81d643dcbbfa373be8c283818dd0eb16
Johannes Heucher <johannes.heucher@gip.com>
2023-08-07 14:29:00 +0200

    setting new patch-version before release

--- version.ts
+++ version.ts
@@ -16,4 +16,4 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 export const ProcessModellerName = 'Process Modeller';
-export const ProcessModellerVersion = '2.0.1';
+export const ProcessModellerVersion = '2.0.2';
