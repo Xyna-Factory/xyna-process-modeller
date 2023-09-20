8efd8ccd626ac576ae2925084f1a2f794c7b5889
johannesheucher-gip <107180330+johannesheucher-gip@users.noreply.github.com>
2023-09-20 15:16:13 +0200

    Update version.ts
    v2.1.0

--- version.ts
+++ version.ts
@@ -16,4 +16,4 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 export const ProcessModellerName = 'Process Modeller';
-export const ProcessModellerVersion = '2.0.2';
+export const ProcessModellerVersion = '2.1.0';
