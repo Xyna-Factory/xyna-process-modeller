c189b5972a6c7ec9b2a8f7d7eea700d07b6a339c
Torben Siegismund <127193495+TorbenSiegismund-GIP@users.noreply.github.com>
2024-01-25 11:39:31 +0100

    Remember choosen mapping mode for each mapping in workflow detail level service.

--- document/workflow-detail-level.service.ts
+++ document/workflow-detail-level.service.ts
@@ -19,10 +19,16 @@ import { Injectable } from '@angular/core';
 import { WorkflowDetailSettingsService } from '@pmod/workflow-detail-settings.service';
 import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
+export enum MappingMode {
+    PROGRAMMATIC = 'programmatic',
+    VISUAL = 'visual'
+}
+
 /**
  * Manages detail lever per Workflow. I. e.
  * - collapsed state for each object's ID, that is collapsable in some way
  * - one flag to display the FQNs throughout the whole Workflow
+ * - mappingMode states for mappings
  */
 @Injectable()
 export class WorkflowDetailLevelService {
@@ -40,6 +46,17 @@ export class WorkflowDetailLevelService {
     private readonly _collapsedAll = new Subject<void>();
 
     private readonly _showFQN: BehaviorSubject<boolean>;
+
+
+    /**
+     * holds the mapping mode state for each mapping by its ID
+     */
+    private readonly _mappingModes = new Map<string, MappingMode>();
+
+    /**
+     * Subject to trigger if mappingMode state changes for an ID
+     */
+    private readonly _mappingModeChange = new Subject<string>();
 
 
     constructor(private readonly workflowSettings: WorkflowDetailSettingsService) {
@@ -106,5 +123,39 @@ export class WorkflowDetailLevelService {
 
     showFQNChange(): Observable<boolean> {
         return this._showFQN.asObservable();
+    }
+
+
+    // -- MappingMode --------------------------------------------
+
+
+    getDefaultMode(): MappingMode {
+        return MappingMode.PROGRAMMATIC;
+    }
+
+    /**
+     * Returns Mode state for an mapping ID
+     * @param id ID of mapping to request mode state for
+     * @return Mode state for mapping ID. If it is not known, getDefaultMode will be returned
+     */
+    getMappingMode(id: string): MappingMode {
+        return this._mappingModes.get(id) ?? this.getDefaultMode();
+    }
+
+    /**
+     * Set Mode state for an mapping ID
+     * @param id ID of mapping to request mode state change for
+     * @param mode Mode of mapping to change to
+     */
+    setMappingMode(id: string, mode: MappingMode) {
+        if (id && this.getMappingMode(id) !== mode) {
+            this._mappingModes.set(id, mode);
+            this._mappingModeChange.next(id);
+        }
+    }
+
+
+    mappingModeChange(): Observable<string> {
+        return this._mappingModeChange.asObservable();
     }
 }
