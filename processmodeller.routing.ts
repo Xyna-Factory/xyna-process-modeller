b5d6408c3ce9dc2e09281cee6238bfbf3fda5803
Julian von Selasinsky <julian.vonselasinsky@outlook.de>
2024-02-12 15:38:15 +0100

    Bug fixing upgrade to Angular 16

--- processmodeller.routing.ts
+++ processmodeller.routing.ts
@@ -18,7 +18,7 @@
 import { RouterModule } from '@angular/router';
 
 import { XynaRoutes } from '@zeta/nav';
-import { RightGuard } from '@zeta/nav/right.guard';
+import { RightGuardCanActivate } from '@zeta/nav/right.guard';
 
 import { RIGHT_PROCESS_MODELLER } from './const';
 import { ProcessmodellerComponent } from './processmodeller.component';
@@ -36,7 +36,7 @@ export const ProcessmodellerRoutes: XynaRoutes = [
     {
         path: root,
         component: ProcessmodellerComponent,
-        canActivate: [RightGuard],
+        canActivate: [RightGuardCanActivate],
         data: {
             right: RIGHT_PROCESS_MODELLER,
             reuse: root,
