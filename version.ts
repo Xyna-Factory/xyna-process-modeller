c150b212ae1939d3b41a47348848403e10da4381
Oliver Sünder <oliver.suender@gip.com>
2024-03-26 14:35:37 +0100

    2.3.0

--- version.ts
+++ version.ts
@@ -16,4 +16,4 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 export const ProcessModellerName = 'Process Modeller';
-export const ProcessModellerVersion = '2.2.0';
+export const ProcessModellerVersion = '2.3.0';
