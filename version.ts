8fd09b61849f727849faef3435ad427fa05e16df
TorbenSiegismund-GIP <torben.siegismund@gip.com>
2024-05-16 11:58:00 +0200

    change version to 2.5.0

--- version.ts
+++ version.ts
@@ -16,4 +16,4 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 export const ProcessModellerName = 'Process Modeller';
-export const ProcessModellerVersion = '2.4.0';
+export const ProcessModellerVersion = '2.5.0';
