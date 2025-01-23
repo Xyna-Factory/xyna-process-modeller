3f5327e4cc728b79381c6c5f92f33eefed5e64c4
Julian-Mueller-GIP <julian.mueller@gip.com>
2025-01-23 09:49:59 +0100

    added overflow property in .lib-area

--- version.ts
+++ version.ts
@@ -16,4 +16,4 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 export const ProcessModellerName = 'Process Modeller';
-export const ProcessModellerVersion = '2.6.9';
+export const ProcessModellerVersion = '2.6.10';
