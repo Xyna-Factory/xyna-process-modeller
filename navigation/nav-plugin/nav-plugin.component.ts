c20cc79e587406ab8965399ad3b8f262d8a3312b
Torben Siegismund <127193495+TorbenSiegismund-GIP@users.noreply.github.com>
2025-02-06 17:11:12 +0100

    144 implement area for plugins in right navbar (#147)

---
+++ navigation/nav-plugin/nav-plugin.component.ts
@@ -0,0 +1,66 @@
+/*
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ * Copyright 2025 Xyna GmbH, Germany
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ */
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, Input } from '@angular/core';
+
+import { CommonNavigationComponent } from '../common-navigation-class/common-navigation-component';
+import { PluginService } from '@pmod/document/plugin.service';
+import { XoDefinitionBundle } from '@zeta/xc/xc-form/definitions/xo/base-definition.model';
+import { XoPlugin } from '@zeta/xc';
+import { XoGuiDefiningWorkflow } from '@yggdrasil/plugin/gui-defining-workflow.model';
+
+
+@Component({
+    selector: 'xfm-mod-nav-plugin',
+    templateUrl: './nav-plugin.component.html',
+    styleUrls: ['./nav-plugin.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NavPluginComponent extends CommonNavigationComponent {
+
+    readonly pluginService: PluginService = inject<PluginService>(PluginService);
+
+    private _plugin: XoPlugin;
+    private _bundle: XoDefinitionBundle;
+
+    get bundle(): XoDefinitionBundle {
+        return this._bundle;
+    }
+
+    @Input('plugin')
+    set plugin(value: XoPlugin) {
+        this._plugin = value;
+        this.requestBundle();
+    }
+
+    @Input('plugin-number')
+    pluginNumber: number;
+
+    constructor(cdr: ChangeDetectorRef) {
+        super(cdr);
+    }
+
+    private requestBundle() {
+        const definingWorkflow: XoGuiDefiningWorkflow = new XoGuiDefiningWorkflow();
+        definingWorkflow.runtimeContext = this._plugin.pluginRTC;
+        definingWorkflow.fQN = this._plugin.definitionWorkflowFQN;
+        definingWorkflow.zetaRTC = this._plugin.pluginRTC.toRuntimeContext();
+        this.pluginService.getFromCacheOrCallWorkflow(definingWorkflow).subscribe({
+            next: (bundle: XoDefinitionBundle) => this._bundle = bundle
+        });
+    }
+}
