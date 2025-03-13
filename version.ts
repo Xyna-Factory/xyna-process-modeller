647174a0d0bfd094e11a3d217b5d429d8b3c94d1
TorbenSiegismund-GIP <torben.siegismund@gip.com>
2025-03-13 10:54:20 +0100

    open error dialog if show result is ture and an error occure.

--- version.ts
+++ version.ts
@@ -16,4 +16,4 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 export const ProcessModellerName = 'Process Modeller';
-export const ProcessModellerVersion = '2.8.0';
+export const ProcessModellerVersion = '2.8.1';
