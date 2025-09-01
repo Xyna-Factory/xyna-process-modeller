6f3a3c399c0cd36dc395a39a4e35db97eaac9630
Sela123 <44436281+Sela1236@users.noreply.github.com>
2025-09-01 09:36:14 +0200

    Remove "Throw" button from left toolbar (#180)
    Co-authored-by: Julian von Selasinsky <julian.vonselasinsky@outlook.de>

--- version.ts
+++ version.ts
@@ -16,4 +16,4 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 export const ProcessModellerName = 'Process Modeller';
-export const ProcessModellerVersion = '2.8.11';
+export const ProcessModellerVersion = '2.8.12';
