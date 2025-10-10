0c86260fe5b2d86cdb04981cbc72433fa9303ab6
Torben Siegismund <127193495+TorbenSiegismund-GIP@users.noreply.github.com>
2025-10-10 10:51:56 +0200

    183 unclear arguments in help lists (#184)

--- version.ts
+++ version.ts
@@ -16,4 +16,4 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 export const ProcessModellerName = 'Process Modeller';
-export const ProcessModellerVersion = '2.8.13';
+export const ProcessModellerVersion = '2.8.14';
