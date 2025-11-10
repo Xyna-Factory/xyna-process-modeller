d2dc30a5bf28fa09e79642472ed1911fc08328a7
Julian von Selasinsky <julian.vonselasinsky@outlook.de>
2025-11-10 11:41:06 +0100

    Prototype variables are no longer displayed with hatching

--- document/workflow/variable/variable.component.ts
+++ document/workflow/variable/variable.component.ts
@@ -338,7 +338,7 @@ export class VariableComponent extends SelectableModellingObjectComponent {
 
     @HostBinding('class.prototype')
     get isAbstract(): boolean {
-        return this.variable?.$fqn ? this.variable.isAbstract : undefined;
+        return this.variable.isAbstract;
     }
 
 
