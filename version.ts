0bf8957d6fba68de9f86759a72108ed8ed16a6a0
TorbenSiegismund-GIP <torben.siegismund@gip.com>
2026-02-19 11:25:52 +0100

    Hide connection lines in conditional branching.

--- version.ts
+++ version.ts
@@ -16,4 +16,4 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 export const ProcessModellerName = 'Process Modeller';
-export const ProcessModellerVersion = '2.8.28';
+export const ProcessModellerVersion = '2.8.29';
