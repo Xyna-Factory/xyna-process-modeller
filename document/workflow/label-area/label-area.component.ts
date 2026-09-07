33bfce666245869f9bea57b92a2285d6fa28bb80
jvs-gip <julian.vonselasinsky@gip.com>
2026-09-07 12:03:07 +0200

    update

--- document/workflow/label-area/label-area.component.ts
+++ document/workflow/label-area/label-area.component.ts
@@ -17,7 +17,7 @@
  */
 
 import { NgTemplateOutlet } from '@angular/common';
-import { Component, Input, TemplateRef, input } from '@angular/core';
+import { Component, Input, input, TemplateRef } from '@angular/core';
 
 import { XoLabelArea } from '../../../xo/label-area.model';
 import { ModContentEditableDirective } from '../shared/mod-content-editable.directive';
