5513ad6c03be172c5fcdc4b3dcba93c09dc503a5
jvs-gip <julian.vonselasinsky@gip.com>
2026-09-11 10:44:55 +0200

    update version

--- version.ts
+++ version.ts
@@ -16,4 +16,4 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 export const ProcessModellerName = 'Process Modeller';
-export const ProcessModellerVersion = '2.8.43';
+export const ProcessModellerVersion = '2.9.0';
